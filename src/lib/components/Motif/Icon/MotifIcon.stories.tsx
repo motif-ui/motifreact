fe7713d75b8b74d5aea1fdf6abc8f356ad1230ec
Mehmet Aktaş <mehmet@mehmetaktas.org>
2026-05-05 10:26:33 +0300

    motif icon fix and icomoon update prepare

---
+++ src/lib/components/Motif/Icon/MotifIcon.stories.tsx
@@ -0,0 +1,20 @@
+import type { Meta, StoryObj } from "@storybook/nextjs";
+import iconMoon from "./assets/selection.icomoon.json" with { type: "json" };
+import MotifIcon from "@/components/Motif/Icon/MotifIcon";
+
+const meta: Meta = {
+  title: "Chromatic/MotifIcon",
+  tags: ["!autodocs", "!dev"],
+  parameters: { layout: "padded" },
+};
+
+export default meta;
+type Story = StoryObj;
+
+export const Icons: Story = {
+  render: () => (
+    <div style={{ width: 440, display: "flex", flexWrap: "wrap", gap: 10 }}>
+      {iconMoon.glyphs.flatMap(icon => icon.extras.ligatures.map(name => <MotifIcon key={name} size="xxl" name={name} />))}
+    </div>
+  ),
+};
