8e1f6264e98f4af19d95282c9846e5835d345598
Mehmet Aktas <mehmet@mehmetaktas.org>
2026-06-01 13:39:05 +0300

    refactor(InputPassword): custom input is replaced (#74)

--- src/lib/components/InputPassword/types.ts
+++ src/lib/components/InputPassword/types.ts
@@ -3,11 +3,11 @@ import { InputCommonProps, InputSize } from "../Form/types";
 
 export type InputPasswordProps = {
   placeholder?: string;
-  icon?: IconGlobalType;
 } & InputCommonProps &
   InputPasswordDefaultableProps;
 
 export type InputPasswordDefaultableProps = {
+  icon?: IconGlobalType;
   toggleMask?: boolean;
   size?: InputSize;
   pill?: boolean;
