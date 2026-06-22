89a8374c5bab2981090915e670d861a22df15a48
ZehranurC <zehranurakky@gmail.com>
2026-06-22 12:38:27 +0300

    fix: fixed chromatic issue

--- src/lib/components/Table/components/Foot/ColumnFootArea.tsx
+++ src/lib/components/Table/components/Foot/ColumnFootArea.tsx
@@ -1,7 +1,7 @@
 import { ReactNode, useContext, useMemo } from "react";
 import { TableContext } from "@/components/Table/TableContext";
 import { Column, RowBackground } from "@/components/Table/types";
-import { getRenderableHeaderColumns, getSpanProps } from "@/components/Table/helper";
+import { getColSpan, getRenderableHeaderColumns, getSpanProps } from "@/components/Table/helper";
 import { getValueByChainedKey } from "../../../../../utils/utils";
 import styles from "../../Table.module.scss";
 
@@ -21,6 +21,25 @@ const ColumnFootArea = ({ background, customFooter }: Props) => {
     [columns, showFixedRowNumbers, selectable],
   );
 
+  const colspanInfoList = useMemo(
+    () =>
+      columnsConsideringExtraCols?.reduceRight(
+        (acc, column, index, arr) => {
+          const columnSpan = getColSpan(column, arr.length - index) ?? 1;
+          if (column.footer) {
+            return [{ colSpan: 0, title: column.footer.title }, ...acc];
+          } else {
+            if (!acc.length) return [{ colSpan: columnSpan }];
+
+            const [lastItem, ...rest] = acc;
+            return [{ colSpan: lastItem.colSpan + columnSpan, title: lastItem.title }, { colSpan: 0 }, ...rest];
+          }
+        },
+        [] as { colSpan: number; title?: string }[],
+      ),
+    [columnsConsideringExtraCols],
+  );
+
   const getFooterValue = ({ title, dataKey, footer }: Column) => {
     const { type } = footer || {};
     switch (type) {
@@ -29,7 +48,9 @@ const ColumnFootArea = ({ background, customFooter }: Props) => {
           ? (originalRows.reduce((acc, row) => acc + getValueByChainedKey<number>(row.data, dataKey), 0) / originalRows.length).toFixed(2)
           : undefined;
       case "sum":
-        return originalRows?.reduce((acc, row) => acc + getValueByChainedKey<number>(row.data, dataKey), 0);
+        return originalRows?.length
+          ? originalRows.reduce((acc, row) => acc + getValueByChainedKey<number>(row.data, dataKey), 0)
+          : undefined;
       case "title":
         return title;
       default:
@@ -38,16 +59,16 @@ const ColumnFootArea = ({ background, customFooter }: Props) => {
   };
 
   const footerCells = columnsConsideringExtraCols
-    ? getRenderableHeaderColumns(columnsConsideringExtraCols).map(({ column, index, colSpan: headerColSpan }) => {
-        const actualColSpan = headerColSpan ?? 1;
-        const footerValue = getFooterValue(column);
-        const footerContent = column.footer ? (column.footer.render?.(footerValue) ?? footerValue) : "";
-        const footerTitle = column.footer?.title;
+    ? getRenderableHeaderColumns(columnsConsideringExtraCols).map(({ column, index }) => {
+        const info = colspanInfoList?.[index];
+        if (!info || (!column.footer && !info.colSpan)) {
+          return null;
+        }
 
+        const { colSpan, title } = info;
         return (
-          <th key={"foot_" + index} {...getSpanProps(actualColSpan)}>
-            {footerTitle && <div>{footerTitle}</div>}
-            {footerContent}
+          <th key={"foot_" + index} {...getSpanProps(colSpan)}>
+            {column.footer ? (column.footer.render?.(getFooterValue(column)) ?? getFooterValue(column)) : colSpan > 0 && title ? title : ""}
           </th>
         );
       })
@@ -55,7 +76,7 @@ const ColumnFootArea = ({ background, customFooter }: Props) => {
 
   return (
     <tfoot className={styles[background]} data-testid="TableFooter">
-      {footerCells && <tr>{footerCells}</tr>}
+      {footerCells && <tr>{footerCells.filter(Boolean)}</tr>}
       {customFooter && (
         <tr>
           <th className={styles.customFooter} colSpan={numberOfVisibleColumns}>
