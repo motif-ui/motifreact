1ef4ac25881ac8ab6fe53d02f34e4afb5c31096a
Mehmet Aktaş <mehmet@mehmetaktas.org>
2026-06-24 10:22:29 +0300

    refactor fixes

--- src/lib/components/NavBar/NavBar.tsx
+++ src/lib/components/NavBar/NavBar.tsx
@@ -36,7 +36,7 @@ const NavBar = (props: PropsWithRef<NavBarProps, HTMLElement>) => {
         {mainMenu && (
           <MotifIconButton name="density_medium" size="xl" onClick={() => setMenuCollapsed(p => !p)} className={styles.hamburger} />
         )}
-        {(logo || logoSlot) && <NavBarLogo {...logo}>{logoSlot}</NavBarLogo>}
+        {(logo || logoSlot) && <NavBarLogo logo={logo}>{logoSlot}</NavBarLogo>}
         <div className={styles.mainMenuContainer}>{mainMenu && <Menu {...mainMenu} main />}</div>
         {search && <NavBarSearch {...search} />}
         {button && <NavBarButton {...button} />}
--- src/lib/components/NavBar/components/NavBarLogo.tsx
+++ src/lib/components/NavBar/components/NavBarLogo.tsx
@@ -2,24 +2,27 @@ import { PropsWithChildren } from "react";
 import styles from "../NavBar.module.scss";
 
 export type NavBarLogoProps = {
-  imgPath?: string;
+  imgPath: string;
   alt?: string;
   href?: string;
 };
 
-const NavBarLogo = ({ children, imgPath, alt, href }: PropsWithChildren<NavBarLogoProps>) => {
-  const logoContent =
-    children ||
-    (imgPath &&
-      (href ? (
-        <a href={href}>
-          <img src={imgPath} alt={alt} />
-        </a>
-      ) : (
-        <img src={imgPath} alt={alt} />
-      )));
-
-  return logoContent ? <div className={styles.logo}>{logoContent}</div> : null;
+const NavBarLogo = (props: PropsWithChildren<{ logo?: NavBarLogoProps }>) => {
+  const { logo, children } = props;
+  const { alt, imgPath, href } = logo || {};
+  return (
+    <div className={styles.logo}>
+      {children ??
+        (logo &&
+          (href ? (
+            <a href={href}>
+              <img src={imgPath} alt={alt} />
+            </a>
+          ) : (
+            <img src={imgPath} alt={alt} />
+          )))}
+    </div>
+  );
 };
 
 export default NavBarLogo;
