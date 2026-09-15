b3450f30b1e3519a5fea8d9c9a64a3e29a7c7c2b
Mehmet Aktas <mehmet@mehmetaktas.org>
2026-09-15 10:24:49 +0300

    fix(Storybook): mobile and responsive view issues  (#154)

--- src/lib/components/Select/Select.stories.tsx
+++ src/lib/components/Select/Select.stories.tsx
@@ -10,7 +10,7 @@ const meta: Meta<typeof Select> = {
   component: Select,
   decorators: [
     Story => (
-      <div style={{ minWidth: 300 }}>
+      <div style={{ padding: "0 0 120px" }}>
         <Story />
       </div>
     ),
@@ -20,7 +20,9 @@ const meta: Meta<typeof Select> = {
     size: { table: { defaultValue: { summary: "md" } } },
     value: { control: { type: "text" }, description: "string or string[] if multiple" },
   },
-  args: {},
+  args: {
+    style: { width: 256 },
+  },
 };
 
 export default meta;
